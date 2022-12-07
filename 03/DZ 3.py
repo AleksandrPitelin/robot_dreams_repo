cf32145b5239ec4347183aff126b9de5a298a909
OleksandrPitelin <partsua@ukr.net>
2022-12-07 14:06:07 +0200

    DZ_3.py

---
+++ 03/DZ 3.py
@@ -0,0 +1,79 @@
+#I love Python*42#
+
+# Zadanie 1
+x = ("I love Python")
+z = x*42
+print(z)
+
+#age_in_month#
+
+# Zadanie 2
+x = str("age_in_month")
+y = 37*12
+z = x+" "+str(y)
+print(z)
+
+#Take age in "y" Zadanie 2#
+
+# Zadanie 3
+i = str(" age_in_years")
+j = str(i)+" " + str(y//12)
+print (j)
+
+#----------------------#
+# Zadanie 4
+first_name = "Oleksandr"
+last_name = "Pitelin"
+full_name = first_name +" "+ last_name
+k = ("Му name is" +" "+ (full_name))
+l = ("I’m" + (j) + "years old")
+my_age =  k+" "+l
+
+print(my_age)
+
+#----------------------#
+# Zadanie 5
+
+a=5>7
+print(a)
+
+a=4<=4
+print(a)
+
+a=52//2<14*2
+print(a)
+
+a=51
+print(a>31 and a<55)
+
+a=12/3>2*7
+print(a)
+
+
+#insert (int) to  (str)#
+# Zadanie 6
+a=str(2)
+b=str(5)
+c=str(6)
+d=(a+b+c)
+print(d)
+
+
+
+
+#----------------------#
+#a = 56
+#Give_me_a_number_1_to_60 = int(input(40))
+#if a < 56:
+#    print ("min")
+#elif a > 56:
+#    print("max")
+#elif a == 56:
+#    print("BINGO!!!")
+
+#--------------------------------#
+#my_age = z
+#year_of_born = x
+#this_year = y
+#year_on_week = (365/7)*z
+#z = y - x
